ea9d716c9cbb43cb27a24c19b5d9f814da7a1444
Nicholas Miller <11604027+NicholasDJM@users.noreply.github.com>
2022-06-20 20:16:18 -0400

    Minor updates

--- HighlightComments.js
+++ HighlightComments.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         Highlight Creator Comments on Webtoons
 // @namespace    https://github.com/NicholasDJM/HighlightCreatorCommentsOnWebtoons
-// @version      0.3
+// @version      0.4
 // @description  Highlights comments made by the creator of the comic.
 // @author       Nicholas Miller
-// @include      https://www.webtoons.com/*
+// @updateURL    https://raw.githubusercontent.com/NicholasDJM/HighlightCreatorCommentsOnWebtoons/main/HighlightComments.js
+// @downloadURL  https://raw.githubusercontent.com/NicholasDJM/HighlightCreatorCommentsOnWebtoons/main/HighlightComments.js
+// @match        https://www.webtoons.com/*
 // @icon         https://icons.duckduckgo.com/ip2/webtoons.com.ico
 // @require      https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js#sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=
 // @grant        GM_log
@@ -24,13 +26,12 @@ if (jQuery.fn.jquery == "3.6.0") {
                 const elem = $(element).parents(".u_cbox_area");
                 elem.css("border", "#00dc64 solid 3px");
                 elem.css("border-radius", "5px");
+                // Detect if Creator comment is a reply:
                 if (elem.parents(".u_cbox_reply_area").length===0) {
                     elem.css("padding", "5px");
                 } else {
-                    // Detect if Creator comment is a reply, and add this instead: 'padding: 17px 5px 17px 33px;'
-                    // Check if u_cbox_area is a child of u_cbox_reply_area
+                    // Check if u_cbox_area is a child of u_cbox_reply_area, then add 'padding: 17px 5px 17px 33px;', and a 10px to top and bottom margins.
                     elem.css("padding", "17px 5px 17px 33px");
-                    // If comment is a reply, also add a margin to the top and bottom
                     elem.css("margin-top", "10px");
                     elem.css("margin-bottom", "10px");
                 }
